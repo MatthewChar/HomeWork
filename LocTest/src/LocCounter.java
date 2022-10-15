38f5ce65596212f7b952a602c5d52e62c47fd2df
Matthew Charu <sunisaposang@gmail.com>
2022-10-15 11:49:44 -0700

    Created Main()

---
+++ LocTest/src/LocCounter.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+
+public class LocCounter {
+	private int lineCount;
+	private int ifElseCount;
+	private int switchCount;
+	private int forCount;
+	private int whileCount;
+	private int breconCount;
+	private int tryCatchCount;
+	private int commentCount;
+
+	public int getLineCount() {
+		return lineCount;
+	}
+
+	public int getIfElseCount() {
+		return ifElseCount;
+	}
+
+	public int getSwitchCount() {
+		return switchCount;
+	}
+
+	public int getForCount() {
+		return forCount;
+	}
+
+	public int getWhileCount() {
+		return whileCount;
+	}
+
+	public int getbreconCount() {
+		return breconCount;
+	}
+
+	public int gettryCatchCount() {
+		return tryCatchCount;
+	}
+
+	public int getCommentCount() {
+		return commentCount;
+	}
+
+	public String LineCount(String codeStr) {
+		commentCount = 0;
+		lineCount = 0;
+		String[] codeHold = codeStr.split("\n|\r");
+		ArrayList<String> newCodeHold = new ArrayList<String>();
+		for (int i = 0; i < codeHold.length; i++) {
+			if (codeHold[i].contains("//") || codeHold[i].contains("/**") || codeHold[i].contains("/*")) {
+				commentCount++;
+			}
+			if (codeHold[i].isBlank() == true || codeHold[i].contains("//") || codeHold[i].contains("/**")
+					|| codeHold[i].contains("/*") || codeHold[i].contains("*") || codeHold[i].contains("*/")) {
+				continue;
+			} else {
+				newCodeHold.add(codeHold[i]);
+				lineCount++;
+			}
+		}
+		// System.out.println(newCodeHold.length);
+		controlCount(newCodeHold);
+		return "" + lineCount;
+	}
+
+	public void controlCount(ArrayList<String> codeHold) {
+		ifElseCount = 0;
+		switchCount = 0;
+		forCount = 0;
+		whileCount = 0;
+		breconCount = 0;
+		tryCatchCount = 0;
+		for (int i = 0; i < codeHold.size(); i++) {
+			if (codeHold.get(i).contains("if (") || codeHold.get(i).contains("if else (")
+					|| codeHold.get(i).contains(" else {")) {
+				ifElseCount++;
+			}
+			if (codeHold.get(i).contains("switch (")) {
+				switchCount++;
+			}
+			if (codeHold.get(i).contains("for (")) {
+				forCount++;
+			}
+			if (codeHold.get(i).contains("while (")) {
+				whileCount++;
+			}
+			if (codeHold.get(i).contains("break;") || codeHold.get(i).contains("continue;")) {
+				breconCount++;
+			}
+			if (codeHold.get(i).contains("try {") || codeHold.get(i).contains("catch (")) {
+				tryCatchCount++;
+			}
+		}
+
+	}
+
+}
---
+++ LocTest/src/LocMain.java
@@ -0,0 +1,111 @@
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class LocMain {
+
+	private JFrame frame;
+	private JButton btnBuild;
+	private JTextArea countText;
+	private JFileChooser fileChooser;
+	private String codeCount;
+	private LocCounter javaCounter = new LocCounter();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LocMain window = new LocMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LocMain() {
+		initialize();
+
+		btnBuild.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fileChooser = new JFileChooser();
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("Java Files", "java");
+				fileChooser.setFileFilter(filter);
+				int returnVal = fileChooser.showOpenDialog(null);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fileChooser.getSelectedFile();
+					Path filePath = Paths.get(file.getPath());
+					try {
+						codeCount = Files.readString(filePath);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				performCount(codeCount);
+			}
+		});
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("Lines of Code Tester");
+		lblNewLabel.setBounds(176, 10, 139, 29);
+		frame.getContentPane().add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Select File to count code for");
+		lblNewLabel_1.setBounds(170, 49, 196, 29);
+		frame.getContentPane().add(lblNewLabel_1);
+
+		btnBuild = new JButton("Count Button");
+		btnBuild.setBounds(176, 102, 110, 21);
+		frame.getContentPane().add(btnBuild);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 133, 416, 120);
+		frame.getContentPane().add(scrollPane);
+
+		countText = new JTextArea();
+		scrollPane.setViewportView(countText);
+	}
+
+	public void performCount(String codeCount) {
+//		String codeAns = "";
+//		codeAns = javaCounter.LineCount(codeCount);
+		String codeHolder = javaCounter.LineCount(codeCount);
+		countText.setText("This is your LOC: " + codeHolder + "\n" + "This is the number of comments you have: "
+				+ javaCounter.getCommentCount() + "\n" + "This is your if, else if, else count: "
+				+ javaCounter.getIfElseCount() + "\n" + "This is your switch count: " + javaCounter.getSwitchCount()
+				+ "\n" + "This is your for loop count: " + javaCounter.getForCount() + "\n"
+				+ "This is your while loop count: " + javaCounter.getWhileCount() + "\n"
+				+ "This is your break and continue count: " + javaCounter.getbreconCount() + "\n"
+				+ "This is your try and catch count: " + javaCounter.gettryCatchCount());
+	}
+}
